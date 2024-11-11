9e394039130f669581aee636b64c65f4a6948dd2
heliobei <suporte@beieducacaopedagogico.com>
2024-11-11 15:03:19 -0300

    Update AccordionContent.jsx

--- app/components/AccordionContent.jsx
+++ app/components/AccordionContent.jsx
@@ -18,24 +18,14 @@ export default function Acc() {
         <Accordion defaultExpandedKeys={["1"]}>
             <AccordionItem key="1" aria-label="Vídeo Tutorial" subtitle="Clique para abrir" title="Vídeo Tutorial" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700 py-4 my-4 bg-slate-200">
             <div className='flex justify-center items-center'>
-                <iframe 
-                    width="560" 
-                    height="315" 
-                    src="https://www.youtube.com/embed/Hw0lZjIxELY" 
-                    title="YouTube video player" 
-                    frameBorder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowFullScreen
-                    
-
-                ></iframe>
+              <iframe src="https://drive.google.com/file/d/1IqaZU0DxJQtXWvQ2Gc8XMLf8rvkqFm8O/preview" width="100%" height="600" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen allow="autoplay; clipboard-read; clipboard-write"></iframe>
             </div>
             </AccordionItem>
             <AccordionItem key="2" aria-label="Caderno do Estudante" subtitle="Clique para abrir" title="Manual de Acesso à Plataforma" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700 py-4 my-4 bg-slate-200">
-             <div className='flex justify-center items-center'>
-                <iframe src="https://beieducacao.com.br/conteudo-aprendendo-a-lidar-com-dinheiro/assets/uploads/roteiro.pdf" width="100%" height="600" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen allow="autoplay; clipboard-read; clipboard-write"></iframe>
-                </div>
-            </AccordionItem>
+            <div className='flex justify-center items-center'>
+              <iframe src="https://drive.google.com/file/d/1W2qDAF5omndVyFoawPBIX35Y3Es3vDEu/preview" width="100%" height="600" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen allow="autoplay; clipboard-read; clipboard-write"></iframe>
+            </div>
+          </AccordionItem>
             
             
         </Accordion>
